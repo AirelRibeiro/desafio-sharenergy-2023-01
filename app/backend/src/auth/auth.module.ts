61d47907e0c0929de58436cd92c41b60f1429e16
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-12 19:00:15 -0300

    feat: imports JwtModule and includes JwtStrategy on providers

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './localAuth/local.strategy';
+import 'dotenv/config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwtAuth/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
