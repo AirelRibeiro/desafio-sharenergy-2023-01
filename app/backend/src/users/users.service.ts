04232296709508d27916318c4689ac08409a165c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-12 15:45:14 -0300

    feat: includes CRUD methods in UserService

--- app/backend/src/users/users.service.ts
+++ app/backend/src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<UserDocument> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  async findAll(): Promise<UserDocument[]> {
+    return this.userModel.find();
+  }
+
+  async findOne(id: string): Promise<UserDocument> {
+    return this.userModel.findById(id);
+  }
+
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserDocument> {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto);
+  }
+
+  async remove(id: string) {
+    return this.userModel.findByIdAndRemove(id);
+  }
+}
