885371119ef2fdc63664eedadac0de341195d0dc
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-12 15:49:10 -0300

    feat: implements CreateUserDto

--- app/backend/src/users/dto/create-user.dto.ts
+++ app/backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  MaxLength,
+  MinLength,
+  Length,
+  IsPhoneNumber,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsPhoneNumber('BR')
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(50)
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(11)
+  cpf: string;
+}
