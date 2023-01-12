11c4c1b3294362e4219459e659ee703fe32b3717
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-12 19:05:03 -0300

    feat: includes token jwt generation

--- app/backend/src/auth/auth.service.ts
+++ app/backend/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserDocument } from 'src/schemas/user.schema';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import { Md5 } from 'md5-typescript';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(
     username: string,
@@ -17,5 +21,12 @@ export class AuthService {
       return user;
     }
     return null;
+  }
+
+  async login(name: string, id: string) {
+    const payload = { name, id };
+    return {
+      token: this.jwtService.sign(payload),
+    };
   }
 }
