33275659781d9c2d727e654191d231cf1d14cbf8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-12 16:34:31 -0300

    feat: implements validateUser

--- app/backend/src/auth/auth.service.ts
+++ app/backend/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserDocument } from 'src/schemas/user.schema';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(
+    username: string,
+    pass: string,
+  ): Promise<UserDocument | null> {
+    const user = await this.usersService.findByUserName(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result as UserDocument;
+    }
+    return null;
+  }
+}
