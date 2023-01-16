564a4eb9395d1c523a19cade440463a9394e2dc4
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-16 13:28:35 -0300

    feat: criates request function to random users

--- app/frontend/src/services/helpers/apiRequests.ts
+++ app/frontend/src/services/helpers/apiRequests.ts
@@ -5,3 +5,10 @@ export async function requestLogin(user: ILogin) {
   const { data } = await axios.post('http://localhost:3001/auth/login', user);
   return data;
 }
+
+export async function requestRandomUsers() {
+  const { data } = await axios.get(
+    'https://randomuser.me/api/?results=100&nat=br'
+  );
+  return data;
+}
