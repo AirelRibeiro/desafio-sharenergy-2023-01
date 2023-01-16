9cbf1da3408d74dc9aa71dd33f8aeaa8c76fa656
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-16 08:34:10 -0300

    feat: login data interface

--- app/frontend/src/services/interfaces/index.ts
+++ app/frontend/src/services/interfaces/index.ts
@@ -1,5 +1,10 @@
 import { MouseEventHandler } from 'react';
 
+export interface ILogin {
+  username: string;
+  password: string;
+}
+
 export interface IAlertProps {
   message: string;
   closeAlert: MouseEventHandler<HTMLButtonElement>;
