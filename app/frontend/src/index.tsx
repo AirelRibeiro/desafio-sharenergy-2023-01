a7b832a3c8d119d4a75d33f2590200d55be53f3f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-16 08:46:01 -0300

    feat: includes router configuration

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './pages/Login';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/users',
+    element: <div>Users</div>,
+  },
+  {
+    path: '/http-cats',
+    element: <div>HTTP Cats</div>,
+  },
+  {
+    path: '/randon-dogs',
+    element: <div>Dogs</div>,
+  },
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
