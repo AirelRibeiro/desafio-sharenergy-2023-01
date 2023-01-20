2dfc3bb874af6622f5aba35dc38b13a0d6ec0459
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-20 02:11:08 -0300

    refactor: function to format user object and enable componentization UsersView
    - commit out of order of implementation

--- app/frontend/src/services/helpers/auxiliaryFunctions.ts
+++ app/frontend/src/services/helpers/auxiliaryFunctions.ts
@@ -1,3 +1,17 @@
+import { IResult, IUsers } from '../interfaces';
+
+export function formatsUsers(users: IResult[]) {
+  return users.map(({ name, login, picture, email, dob }) => ({
+    id: login.uuid,
+    name: `${
+      name.title === 'Mrs' ? 'Sra.' : name.title === 'Mr' ? 'Sr.' : 'Srta.'
+    } ${name.first} ${name.last}`,
+    username: login.username,
+    email,
+    age: dob.age,
+    image: picture.thumbnail,
+  }));
+}
 
 export function filterUsers(users: IUsers[], filter: string, functions: any) {
   const filteredUsers = [...users].filter((u) => {
