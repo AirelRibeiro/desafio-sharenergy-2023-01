73efa93ea52480040b25a1bd2228e852f28523b1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-01-20 23:44:39 -0300

    feat: includes CRUD requests

--- app/frontend/src/services/helpers/apiRequests.ts
+++ app/frontend/src/services/helpers/apiRequests.ts
@@ -1,8 +1,58 @@
 import axios from 'axios';
-import { ILogin } from '../interfaces';
+import {
+  IClientCreateFields,
+  IClientUpdateFields,
+  ILogin,
+} from '../interfaces';
+import { recoverData } from './storageFunctions';
 
 export async function requestLogin(user: ILogin) {
   const { data } = await axios.post('http://localhost:3001/auth/login', user);
+  return data;
+}
+
+export async function requestCreateClient(client: IClientCreateFields) {
+  const token = recoverData()?.token;
+  const { data } = await axios.post('http://localhost:3001/users', client, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return data;
+}
+
+export async function requestUpdateClient(client: IClientUpdateFields) {
+  const token = recoverData()?.token;
+  const data = await axios.patch(
+    `http://localhost:3001/users/${client._id}`,
+    client,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  return data;
+}
+
+export async function requestClients() {
+  const token = recoverData()?.token;
+  const { data } = await axios.get('http://localhost:3001/users', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return data;
+}
+
+export async function requestSingleClient(id: string) {
+  const token = recoverData()?.token;
+  const { data } = await axios.get(`http://localhost:3001/users/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return data;
+}
+
+export async function requestDeletClient(id: string) {
+  const token = recoverData()?.token;
+  const { data } = await axios.delete(`http://localhost:3001/users/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return data;
 }
 
